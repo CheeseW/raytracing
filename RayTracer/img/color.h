9d3c56f4994fb296ef8eec79d119b90750581f99
CheeseW <qisi.wang@gmail.com>
2021-06-14 13:36:59 -0500

    Minor bug fix for imageWriter

--- RayTracer/img/color.h
+++ RayTracer/img/color.h
@@ -6,7 +6,7 @@
 namespace rayUtilities {
 	using Color = Eigen::Matrix<double, 3, 1>;
 
-	void write_color(std::ofstream& out, Color color, const int nSample) {
+	void write_color(std::ostream& out, Color color, const int nSample) {
 		const auto pixelColor = (color / (double)nSample).cwiseSqrt().cwiseMin(.999).cwiseMax(0.);
 
 		out << static_cast<int> (256 * pixelColor[0]) <<" "<<
